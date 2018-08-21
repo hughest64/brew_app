e1240ecc2333c182397ef93ceefe6ad88db5e9b0
hughest64 <hughest64@gmail.com>
2018-08-20 20:55:03 -0500

    added SessionForm model form

--- recipes/forms.py
+++ recipes/forms.py
@@ -5,3 +5,9 @@ from . import models
 
 class UploadXMLFileForm(forms.Form):
     file = forms.FileField()
+
+
+class SessionForm(forms.ModelForm):
+    class Meta:
+        model = models.Session
+        fields = '__all__'
